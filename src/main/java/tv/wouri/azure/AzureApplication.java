df29fdfc2fed534f613a99a4676853c8b1a59f9c
Sumbang Christian <tsumbang@outlook.com>
2023-03-24 08:19:13 -0400

    update

--- src/main/java/tv/wouri/azure/AzureApplication.java
+++ src/main/java/tv/wouri/azure/AzureApplication.java
@@ -3,7 +3,9 @@ package tv.wouri.azure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,7 +14,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.adc.sygem.models"})
 @EnableJpaRepositories(basePackages = {"tv.wouri.azure.repositories"})
 @ComponentScan({"tv.wouri.azure.controllers","tv.wouri.azure.services","tv.wouri.azure.security"})
-public class AzureApplication {
+public class AzureApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AzureApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureApplication.class, args);
